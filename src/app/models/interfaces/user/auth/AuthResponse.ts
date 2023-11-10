a3df7692fe6b3eeafdcf504424cb6c5890b5ac54
Arlindo Silva <arlindovicente@gmail.com>
2023-11-09 22:38:48 -0300

    login
    camada de serviço da tela de login

---
+++ src/app/models/interfaces/user/auth/AuthResponse.ts
@@ -0,0 +1,6 @@
+export interface AuthResponse {
+  id: string;
+  email: string;
+  password: string;
+  token: string;
+}
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   loginCard = true;
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  signupForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+  }
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do Formulario de Login: ',this.loginForm.value);
+  }
+
+  onSubmitSignupForm(): void {
+    console.log('Dados do Formulario de Criação: ',this.signupForm.value);
+  }
 }
