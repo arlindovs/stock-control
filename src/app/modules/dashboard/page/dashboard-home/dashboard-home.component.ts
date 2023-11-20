39b7f4832fd610516f8f9b323edadf7399919e22
Arlindo Silva <arlindovicente@gmail.com>
2023-11-19 21:57:19 -0300

    implementado grafico de produtos

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,44 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
 import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 
+/**
+ * Componente responsável por exibir a página inicial do painel de controle.
+ */
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class DashboardHomeComponent implements OnInit {
+export class DashboardHomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
+  /**
+   * Lista de produtos.
+   */
   public productList: Array<GetAllProductsResponse> = [];
 
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
 
   constructor(
     private productService: ProductsService,
     private messageService: MessageService,
     private productsDtService: ProductsDataTransferService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getProductsDatas();
   }
+
+  /**
+   * Obtém os dados dos produtos.
+   */
   getProductsDatas(): void {
-    this.productService.gerAllProducts().subscribe({
+    this.productService
+      .gerAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
         next: (response) => {
-        if(response.length > 0){
-          this.productList = response;
-          this.productsDtService.setProductsDatas(this.productList);
-        }
-      }, error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro',
-          detail: 'Erro ao obter os dados dos produtos.',
-          life: 3000,
-        });
-      }
-  })
+          if (response.length > 0) {
+            this.productList = response;
+            this.productsDtService.setProductsDatas(this.productList);
+            this.setProductsChartConfig();
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao obter os dados dos produtos.',
+            life: 3000,
+          });
+        },
+      });
   }
 
+  setProductsChartConfig(): void {
+    if (this.productList.length > 0) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue(
+        '--text-color-secondary'
+      );
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.productsChartDatas = {
+        labels: this.productList.map((element) => element?.name),
+        datasets: [
+          {
+            label: 'Quantidade',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor:
+              documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productList.map((element) => element?.amount),
+          },
+        ],
+      };
+
+      this.productsChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: '500',
+              },
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,11 +1,12 @@
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { UserService } from 'src/app/services/usuario/user.service';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 /**
  * Componente responsável pela página inicial da aplicação.
@@ -15,7 +16,8 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   /**
    * Flag que indica se o card de login está visível.
    */
@@ -54,7 +56,11 @@ export class HomeComponent implements OnInit {
    */
   onSubmitLoginForm(): void {
     if (this.loginForm.value && this.loginForm.valid) {
-      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
         next: (response) => {
           if (response) {
             this.cookieService.set('token', response?.token);
@@ -92,6 +98,9 @@ export class HomeComponent implements OnInit {
     if (this.signupForm.value && this.signupForm.valid) {
       this.userService
         .signupUser(this.signupForm.value as SignupUserRequest)
+        .pipe(
+          takeUntil(this.destroy$)
+        )
         .subscribe({
           next: (response) => {
             if (response) {
@@ -119,5 +128,15 @@ export class HomeComponent implements OnInit {
           },
         });
     }
+  }
+
+
+  /**
+   * Lifecycle hook that is called when the component is about to be destroyed.
+   * It is used to clean up any resources or subscriptions before the component is removed from the DOM.
+   */
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
